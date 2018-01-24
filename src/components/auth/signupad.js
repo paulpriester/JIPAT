e46c844b3f00d159646ede4fd6ce48de548f9105
paulpriester <paulpriester507@gmail.com>
2018-01-23 19:48:17 -0500

    update

---
+++ src/components/auth/signupad.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form'; 
+import {connect} from 'react-redux';
+import * as actions from '../../actions';
+import {Link} from 'react-router';
+
+const renderInput=field=>{
+	const {meta: {touched,error}}=field;
+	return(
+		<div>
+			<input {...field.input} type={field.type} className="form-control" />
+			<div className="error">{touched?error:''}</div>
+		</div>
+		);	
+	
+};
+
+class SignUpAd extends Component{
+
+	handleFormSubmit(formProps){
+		//call action creator to signup user
+		this.props.signUpAdmin(formProps);
+	}
+	renderAlert(){
+		if(this.props.errorMessage){
+			return(
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+				);
+		}
+	}
+	render(){
+		const {handleSubmit}=this.props;
+		
+		return(
+				<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+					<fieldset className="form-group">
+						<label>Email:</label>
+						<Field
+							name="email"
+							component={renderInput}
+							type="email"
+						/>
+					</fieldset>
+					<fieldset className="form-group">
+						<label>Password:</label>
+						<Field
+							name="password"
+							component={renderInput}
+							type="password"
+						/>
+					</fieldset>
+					<fieldset className="form-group">
+						<label>Confirm Password:</label>
+						<Field
+							name="passwordConfirm"
+							component={renderInput}
+							type="password"
+						/>
+					</fieldset>
+					{this.renderAlert()}
+					<button action="submit" className="btn btn-primary">Sign up</button>
+				</form>
+		);
+	}
+}
+function validate(formProps){
+	const errors={};
+	if(!formProps.email)
+		errors.email="Please Enter a Email";
+	if(!formProps.password)
+		errors.password="Please Enter a Password";
+	if(!formProps.passwordConfirm)
+		errors.passwordConfirm="Please Enter a Confirm Password";
+	if(formProps.password!=formProps.passwordConfirm)
+		errors.password="Passwords must match";
+	return errors;
+}
+function mapStateToProps(state){
+	return {errorMessage: state.auth.error};
+}
+
+export default reduxForm({
+	validate,
+	form: 'signupad'
+})(
+	connect(mapStateToProps,actions)(SignUpAd)
+);
---
+++ src/components/invite.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form'; 
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
+const renderInput= field => <input {...field.input} type={field.type} className="form-control" />;
+
+class Invite extends Component{
+
+	handleFormSubmit({email,name, admin}){
+		console.log(email, name, admin);
+		this.props.inviteUser({email, name, admin});
+	}
+	renderAlert(){
+		if(this.props.errorMessage){
+			return(
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+				);
+		}
+	}
+	render(){
+		const {handleSubmit}=this.props;
+		
+		return(
+				<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+					<fieldset className="form-group">
+						<label>Email:</label>
+						<Field
+							name="email"
+							component={renderInput}
+							type="email"
+						/>
+					</fieldset>
+					<fieldset className="form-group">
+						<label>name:</label>
+						<Field
+							name="name"
+							component={renderInput}
+							type="string"
+						/>
+					</fieldset>
+					<fieldset className="form-group">
+						<label>Admin:</label>
+						<Field
+							name="admin"
+							component="input"
+							type="checkbox"
+						/>
+					</fieldset>
+					<button action="submit" className="btn btn-primary">Invite</button>
+				</form>
+		);
+	}
+}
+
+
+
+export default reduxForm({
+	form: 'invite',
+	initialValues: {
+    admin: false // if you want it to default to the first one
+  }
+})(
+	connect(null,actions)(Invite)
+);
