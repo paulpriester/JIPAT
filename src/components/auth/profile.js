be22cca13005a6f49c8b6d257138256cf8dae652
Jermaine <maine1191@gmail.com>
2018-05-17 17:23:07 -0400

    reworked css for student profile

--- src/components/auth/profile.js
+++ src/components/auth/profile.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
 import { connect } from "react-redux";
-import { Grid, Row, Col, Table } from 'react-bootstrap';
 import {fetchProfile, fetchcaselength, fetchSavedSkills} from '../../actions';
 import Dashboard from '../student/dashboard';
 import ModalProfile from '../modal_profile';
 import ModalSkill from '../modal_skill';
+import '../../../public/css/student-profile.css'
 
 
 class Profile extends Component{
@@ -29,45 +29,70 @@ class Profile extends Component{
   render(){
     console.log(this.props);
     return(
-      <div>
-        <Grid fluid className="grid">
-          <Row className="show-grid-head">
-            <Col className="col-box" xs={3} md={3}>
-              <img className="profile-pic" src="https://www.bing.com/th?id=A5e81ae1f568f729aa4810dde8c952a58&w=110&h=110&c=7&rs=1&qlt=80&pcl=f9f9f9&cdv=1&pid=16.1" />
-            </Col>
-            <Col className="col-box" xs={3} md={3}>
-              <h3>Name:</h3>
-              <p>{this.props.information.firstName} {this.props.information.lastName}</p>
-            </Col>
-            <Col className="col-box" xs={3} md={3}>
-              <h3>Github:</h3>
-              <p>{this.props.information.github}</p>
-            </Col>
-            <Col className="col-box" xs={3} md={3}>
-              <h3>Jobs Applied</h3>
-              <div className="jobs col-box">{this.props.caselength.length}</div>
-            </Col>
-          </Row>
-          <Row className="show-grid">
-            <Col xs={6} md={6} className="right-border">
+      <div className="student-profile-container">
+
+              {/* <img className="profile-pic" src="https://www.bing.com/th?id=A5e81ae1f568f729aa4810dde8c952a58&w=110&h=110&c=7&rs=1&qlt=80&pcl=f9f9f9&cdv=1&pid=16.1" /> */}
+          
+            <div className="student-profile-job-info">
+
+                <h3>Name:</h3>
+                <p>{this.props.information.firstName} {this.props.information.lastName}</p>
+            
+            
+                <h3>Github:</h3>
+                <p>{this.props.information.github}</p>
+          
+              <div className="student-applied">
+                <h3>Jobs Applied</h3>
+                <div className="applied-number">{this.props.caselength.length}</div>
+
+              </div>
+
+           </div>
+    
+        <div className="student-about">
+
+          <div className="about-me-skills">
               <h3>About Me</h3>
+              
               <p> {this.props.information.about}    </p>
-            </Col>
-            <Col xs={6} md={6}>
-              <h3>Career Goals</h3>
-              <p> {this.props.information.careergoals} </p>
-            </Col>
-          </Row>
-          <Row className="show-grid">
-            <ModalProfile />
-            <ModalSkill />
+ 
+
+              <h3>Skills</h3>
+               
             {this.props.skill.map(this.renderSkill)}
-            <Col xs={12} md={12}>
+
+            <div className="student-profile-btn-row">
+              <ModalProfile />
+              <ModalSkill />
+              </div>
+            
+
+          </div>
+
+            <div className="student-career-goals">
+
+                <h3>Career Goals</h3>
+                <p> {this.props.information.careergoals} </p>
+
+            </div>
+
+          </div>
+
+          <div className="student-jobs-detail">
+
+          <h3>Jobs Open</h3>
+          <h3>Jobs Closed</h3>
+          <h3>Job Interviews</h3>
+          <h3>Job Interviews 2</h3>
+          <h3>Jobs Placed</h3>
+
+          
+          </div>
+         
               <h3>Cases</h3>
               <Dashboard />
-            </Col>
-          </Row>
-        </Grid>
+       
       </div>
     )
   }
